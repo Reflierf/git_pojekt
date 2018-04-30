38611db7b8b20494ad7d2fef587ff5f0663c4bb8
Reflierf <ref@freemail.hu>
2018-04-30 16:18:12 +0200

    Módosítottam és bővítettem a munka.php-t.

--- munka.php
+++ munka.php
@@ -10,11 +10,11 @@
 
     $a = 105;
     $b = 17;
-    $c = 12;
-	$d = 34;
+    $c = 120;
+    $d = 36;
     print"<br />$a+$b+$c+$d = ".$a+$b+$c+$d."<br />";
 
-    
+   //A GitHub-on szerkesztettem. 
     
     
     //Kivonás
